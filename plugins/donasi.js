72f511b48b39e405eca3d8aa3c226fe259b61c3d
Botzwea <104355180+Botzwea@users.noreply.github.com>
2022-05-21 20:31:09 +0700

    donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
 ┌〔 Donasi • Emoney 〕
-├ PULSA TRI3 : 6289625556161
+├ PULSA TRI3 : 62895324080019
 ├ OVO & DANA : 6289625556161
 └────
 `.trim(), footer, 'Owner', '.owner')
